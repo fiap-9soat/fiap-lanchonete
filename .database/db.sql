a990389eb6262cabf1ec7425b10c44e156bd7435
Ikei <du_ikei@hotmail.com>
2024-09-24 21:09:26 -0300

    feat: added 3 tables (clientes, alimentos and pedidos)

--- .database/db.sql
+++ .database/db.sql
@@ -2,18 +2,45 @@ CREATE DATABASE IF NOT EXISTS lanchonete;
 
 USE lanchonete;
 
-DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS Clientes;
 
-CREATE TABLE employees (
-    emp_no      INT             NOT NULL,  -- UNSIGNED AUTO_INCREMENT??
-    birth_date  DATE            NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,  -- Enumeration of either 'M' or 'F'  
-    hire_date   DATE            NOT NULL,
-    PRIMARY KEY (emp_no)                   -- Index built automatically on primary-key column
-                                           -- INDEX (first_name)
-                                           -- INDEX (last_name)
+CREATE TABLE Clientes (
+    codigo_cliente INT NOT NULL AUTO_INCREMENT,
+    cpf CHAR(11) NOT NULL,
+    nome VARCHAR(14) NOT NULL,
+    sobrenome VARCHAR(16) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (codigo_cliente)
+);
+CREATE UNIQUE INDEX idx1 ON Clientes(cpf);
+
+CREATE TABLE Alimentos (
+    codigo_alimento INT NOT NULL,
+    codigo_tipo_alimento INT NOT NULL,
+    nome_tipo_alimento VARCHAR(30) NOT NULL,
+    nome_alimento VARCHAR(30) NOT NULL,
+    nome_funci_alter VARCHAR(20) NOT NULL,
+    ts_alter TIMESTAMP NOT NULL,
+    PRIMARY KEY (codigo_alimento)
+);
+CREATE UNIQUE INDEX idx2 ON Alimentos(nome_tipo_alimento, nome_alimento);
+
+CREATE TABLE Pedidos (
+    codigo_pedido INT NOT NULL,
+    codigo_cliente INT,
+    codigo_alimento INT,
+    ts_pedido TIMESTAMP NOT NULL,
+    PRIMARY KEY (codigo_pedido, codigo_cliente, codigo_alimento),
+    FOREIGN KEY (codigo_cliente) 
+        REFERENCES Clientes(codigo_cliente) ON UPDATE CASCADE,
+    FOREIGN KEY (codigo_alimento) 
+        REFERENCES Alimentos(codigo_alimento) ON UPDATE CASCADE
 );
 
-INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date) VALUES (1, "2018-07-21", 'João', 'Fulano', 'M', '2018-07-21"');
+INSERT INTO Clientes (cpf, nome, sobrenome, email) VALUES ("12345678910", "João", "da Silva", "joaodasilva@abc.com.br");
+
+
+INSERT INTO Alimentos (codigo_alimento, codigo_tipo_alimento, nome_tipo_alimento, nome_alimento, nome_funci_alter, ts_alter) VALUES (1, 1, "Lanche", "X-burger", "João", CURRENT_TIMESTAMP());
+
+
+INSERT INTO Pedidos (codigo_pedido, codigo_cliente, codigo_alimento, ts_pedido) VALUES (1, 1, 1, CURRENT_TIMESTAMP());
