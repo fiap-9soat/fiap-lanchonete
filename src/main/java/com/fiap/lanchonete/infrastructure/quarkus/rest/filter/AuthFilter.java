c188ee8cc716a1361c764c36ba25a66463634f9c
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-03-22 23:54:25 -0300

    - feat(aws): remove dependencias da AWS - feat(auth): modifica decodificação do token no AuthFilter - fix(pedidos): corrige erros relacionados ao cancelamento de pedido - fix(db): adiciona constraint faltante na tabela de clientes - feat(docs): melhorias do README e criação das coleções Postman/Bruno

--- src/main/java/com/fiap/lanchonete/infrastructure/quarkus/rest/filter/AuthFilter.java
+++ src/main/java/com/fiap/lanchonete/infrastructure/quarkus/rest/filter/AuthFilter.java
@@ -1,39 +1,21 @@
 package com.fiap.lanchonete.infrastructure.quarkus.rest.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiap.lanchonete.domain.model.Cliente;
 import com.fiap.lanchonete.domain.model.UsuarioAutenticado;
 import com.fiap.lanchonete.domain.pojo.CreateClienteDto;
 import com.fiap.lanchonete.domain.ports.in.ClienteService;
-import com.fiap.lanchonete.infrastructure.cognito.service.CognitoClient;
 import com.fiap.lanchonete.infrastructure.quarkus.rest.dto.JwtDto;
-
-import io.quarkus.oidc.token.propagation.reactive.AccessTokenRequestReactiveFilter;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.NotFoundException;
-import jakarta.ws.rs.client.ClientRequestContext;
-import jakarta.ws.rs.client.ClientRequestFilter;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
-
-import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
-import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.reactive.RestResponse;
-import org.jboss.resteasy.reactive.server.ServerRequestFilter;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.GetUserResponse;
-
-import java.util.Base64;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.logging.LogManager;
 
 @Provider
 @RegisterClientHeaders
@@ -41,9 +23,6 @@ import java.util.logging.LogManager;
 public class AuthFilter implements ContainerRequestFilter {
 
     private final Logger logger = Logger.getLogger(AuthFilter.class);
-
-    @Inject
-    CognitoClient cognitoClient;
 
     @Inject
     ClienteService clienteService;
@@ -55,17 +34,19 @@ public class AuthFilter implements ContainerRequestFilter {
     public void filter(ContainerRequestContext requestContext) {
         String token = requestContext.getHeaderString("Authorization");
 
+        if (token == null || token.isEmpty()) return;
+
         JwtDto jwt = decodeJwt(token);
 
         String cpf = jwt.getCustomCpf();
         String email = jwt.getEmail();
         String username = jwt.getCognitoUsername();
-        String codigoUsuario = jwt.getSub();
+        String codigoUsuarioProvedor = jwt.getSub();
 
         usuarioAutenticado.setCpf(cpf);
         usuarioAutenticado.setEmail(email);
         usuarioAutenticado.setUsername(username);
-        usuarioAutenticado.setCodigoUsuarioProvedor(codigoUsuario);
+        usuarioAutenticado.setCodigoUsuarioProvedor(codigoUsuarioProvedor);
 
         this.processaCadastroCliente();
     }
@@ -75,8 +56,7 @@ public class AuthFilter implements ContainerRequestFilter {
             Cliente cliente = clienteService.consultarClientePorCpf(usuarioAutenticado.getCpf());
             usuarioAutenticado.setCodigoCliente(cliente.getCodigoCliente());
             return;
-        } catch (NotFoundException ignored) {
-        }
+        } catch (NotFoundException ignored) {}
 
         // Usuario não cadastrado
         CreateClienteDto createClienteDto = new CreateClienteDto(
@@ -90,32 +70,25 @@ public class AuthFilter implements ContainerRequestFilter {
         usuarioAutenticado.setCodigoCliente(codigoClienteCadastrado);
     }
 
+    /**
+     * Atenção: essa função *não* verifica a validade do JWT, já que assumimos que esse processo já foi realizado
+     * na camada de API Gateway.
+     * @param jwtString
+     * @return
+     */
     public JwtDto decodeJwt(String jwtString) {
-        String[] parts = jwtString.split("\\."); // Split into header, payload, and signature
-        if (parts.length < 2) {
-            logger.info("Invalid JWT format.");
-            return new JwtDto();
-        }
+        DecodedJWT jwt = JWT.decode(jwtString);
 
-        String header = new String(Base64.getUrlDecoder().decode(parts[0]));
-        String payload = new String(Base64.getUrlDecoder().decode(parts[1]));
-
-        JwtDto jwtPayload = JsonToObjectExample(payload);
-
-        logger.info("Header: " + header);
-        logger.info("JwtPayload: " + jwtPayload);
-        return jwtPayload;
+        return jwtPayloadToDTO(jwt.getPayload());
     }
 
-    public JwtDto JsonToObjectExample(String jsonString) {
+    public JwtDto jwtPayloadToDTO(String jsonString) {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            JwtDto jwtPayload = objectMapper.readValue(jsonString, JwtDto.class);
-
-            return jwtPayload;
+            return objectMapper.readValue(jsonString, JwtDto.class);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
         }
 
         return new JwtDto();
